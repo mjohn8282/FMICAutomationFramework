f0ca407fe0d9a802e4f0082c6ef18911127ef4f1
John <smoses1994@gmail.com>
2021-08-05 10:05:45 +0530

    Commit

--- testCases/Login.py
+++ testCases/Login.py
@@ -7,7 +7,7 @@ def login(setup):
     logs.loggen().info("************ Verifying Homepage Title ***********")
     logs.loggen().info("************ Verifying " + inspect.stack()[0][3] + "***********")
     driver = setup
-    driver.get("https://fmicdev.dat.com/Login.aspx")
+    driver.get("https://benchmarktest.dat.com/")
     lp = Login(driver)
     lp.setUsername("john.moses@dat.com")
     lp.setPassword("Dat@1794")
